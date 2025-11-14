8fd723e83cff2e9ab4a166d7a80ab806831aefcb
Lee.edu <ST10485444@rcconnect.edu.za>
2025-11-14 17:28:05 +0200

    Done

---
+++ ICE-Task 5.1/jscript.js
@@ -0,0 +1,68 @@
+const currentTime = document.querySelector("#time")
+const content = document.querySelector(".content")
+const selectMenu = document.querySelectorAll("select")
+const setAlarmBtn = document.querySelector("button")
+
+let alarmTime;
+let isAlarmSet;
+let ringtone = new Audio("ringtone.mp3");
+
+
+for(let i = 12; i > 0; i--){
+    i = i < 10 ? `0${i}` : i
+    let option = `<option value="${i}">${i}</option>`
+    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option)
+}
+for(let i = 59; i >= 0; i--){
+    i = i < 10 ? `0${i}` : i
+    let option = `<option value="${i}">${i}</option>`
+    selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option)
+}
+for(let i = 2; i > 0; i--){
+    let ampm = i == 1 ? "AM" : "PM"
+    let option = `<option value="${ampm}">${ampm}</option>`
+    selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option)
+}
+setInterval(() => {
+    let date = new Date(),
+    h = date.getHours(),
+    m = date.getMinutes(),
+    s = date.getSeconds(),
+    ampm = "AM"
+    if(h >= 12){
+        h = h - 12
+        ampm = "PM"
+    }
+    h = h == 0 ? h = 12 : h
+    h = h < 10 ? "0" + h : h
+    m = m < 10 ? "0" + m : m
+    s = s < 10 ? "0" + s : s
+    currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
+    if(alarmTime == `${h}:${m} ${ampm}`){
+        ringtone.play()
+        ringtone.loop = true;
+    }
+});
+
+const setAlarm = () => {
+    if(isAlarmSet){
+        alarmTime = "";
+        ringtone.pause();
+        content.classList.remove("disabled");
+        isAlarmSet = false;
+        setAlarmBtn.innerText = "Set Alarm";
+        return;
+    }
+
+    let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
+    if(time.includes("Hour") || time.includes("Minute") || time.includes("AM/PM")){
+        alert("Please select a valid time to set Alarm!");
+        return;
+    }
+
+    alarmTime = time;
+    isAlarmSet = true;
+    setAlarmBtn.innerText = "Clear Alarm";
+    content.classList.add("disabled");
+};
+setAlarmBtn.addEventListener("click", setAlarm);
